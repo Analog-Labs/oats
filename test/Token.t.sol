58f0ef6ed012d37c4719b0294cfab3eb53c5db0c
Alexander Gryaznov <hi@agryaznov.com>
2025-04-30 19:58:05 +0300

    refactor tests

--- test/Token.t.sol
+++ test/Token.t.sol
@@ -4,22 +4,22 @@ import {Token} from "../src/Token.sol";
 import {IGateway} from "@analog-gmp/interfaces/IGateway.sol";
 import {ERC20Capped} from "@openzeppelin/token/ERC20/extensions/ERC20Capped.sol";
 import {IERC20Errors} from "@openzeppelin/token/ERC20/ERC20.sol";
-import {Test, console} from "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 
 contract TokenTest is Test {
     Token public token;
-    // Anvil 1st dev account
-    address constant OWNER = 0x7FA9385bE102ac3EAc297483Dd6233D62b3e1496;
-    address constant GATEWAY = address(0x42);
-    uint256 constant CAP = 1_000_000;
-    uint16 constant NETWORK_A = 1;
-    uint16 constant NETWORK_B = 2;
-    address constant TOKEN_A = address(0x01);
-    address constant TOKEN_B = address(0x02);
-    address constant USER_A = address(0x03);
-    address constant USER_B = address(0x04);
 
-    // Mocked responses
+    address constant OWNER = address(0x01);
+    address constant USER = address(0x02);
+    address constant TOKEN = address(0x03);
+    address constant GATEWAY = address(0x42);
+
+    uint256 constant CAP = 1_000_000;
+    uint256 constant AMOUNT = 100500;
+
+    uint16 constant NETWORK = 2;
+    bytes32 constant MSG_ID = bytes32(uint256(0xff));
+    // Mocked response
     uint256 constant COST = 42;
 
     struct TransferCmd {
@@ -33,66 +33,64 @@ contract TokenTest is Test {
         // Mock message cost
         vm.mockCall(GATEWAY, abi.encodeWithSelector(IGateway.estimateMessageCost.selector), abi.encode(COST));
         // Mock submit message
-        vm.mockCall(
-            GATEWAY, abi.encodeWithSelector(IGateway.submitMessage.selector), abi.encode(bytes32(uint256(0xff)))
-        );
+        vm.mockCall(GATEWAY, abi.encodeWithSelector(IGateway.submitMessage.selector), abi.encode(MSG_ID));
     }
 
     function test_Cost() public view {
-        assertEq(token.cost(NETWORK_A), COST);
+        assertEq(token.cost(NETWORK), COST);
     }
 
     function test_Recieve() public {
         assertEq(token.balanceOf(OWNER), CAP / 2);
         assertEq(token.totalSupply(), CAP / 2);
-        assertEq(token.balanceOf(USER_A), 0);
+        assertEq(token.balanceOf(USER), 0);
 
-        bytes memory data = abi.encode(TransferCmd({from: USER_B, to: USER_A, amount: 100500}));
-        bytes32 token_b = bytes32(uint256(uint160(TOKEN_B)));
+        bytes memory data = abi.encode(TransferCmd({from: OWNER, to: USER, amount: AMOUNT}));
+        bytes32 token_b = bytes32(uint256(uint160(TOKEN)));
 
         vm.expectRevert(bytes("Unauthorized: only the gateway can call this method"));
-        token.onGmpReceived(bytes32(uint256(0xff)), NETWORK_B, token_b, 0, data);
+        token.onGmpReceived(MSG_ID, NETWORK, token_b, 0, data);
 
         vm.prank(GATEWAY);
         vm.expectRevert(bytes("Transfer from unknown network"));
-        token.onGmpReceived(bytes32(uint256(0xff)), NETWORK_B, token_b, 0, data);
+        token.onGmpReceived(MSG_ID, NETWORK, token_b, 0, data);
 
         vm.prank(OWNER);
-        token.set_network(NETWORK_B, TOKEN_B);
+        token.set_network(NETWORK, TOKEN);
 
         vm.prank(GATEWAY);
-        token.onGmpReceived(bytes32(uint256(0xff)), NETWORK_B, token_b, 0, data);
+        token.onGmpReceived(MSG_ID, NETWORK, token_b, 0, data);
 
-        assertEq(token.balanceOf(USER_A), 100500);
-        assertEq(token.totalSupply(), CAP / 2 + 100500);
+        assertEq(token.balanceOf(USER), AMOUNT);
+        assertEq(token.totalSupply(), CAP / 2 + AMOUNT);
 
-        data = abi.encode(TransferCmd({from: USER_B, to: USER_A, amount: CAP / 2}));
+        data = abi.encode(TransferCmd({from: OWNER, to: USER, amount: CAP / 2}));
         vm.prank(GATEWAY);
         vm.expectPartialRevert(ERC20Capped.ERC20ExceededCap.selector);
-        token.onGmpReceived(bytes32(uint256(0xff)), NETWORK_B, token_b, 0, data);
+        token.onGmpReceived(MSG_ID, NETWORK, token_b, 0, data);
     }
 
     function test_Send() public {
         assertEq(token.balanceOf(OWNER), CAP / 2);
-        assertEq(token.balanceOf(USER_A), 0);
+        assertEq(token.balanceOf(USER), 0);
 
-        vm.prank(USER_A);
+        vm.prank(USER);
         vm.expectRevert(bytes("Unknown token on target network"));
-        token.send(NETWORK_B, USER_B, 100500);
+        token.send(NETWORK, OWNER, AMOUNT);
 
         vm.prank(OWNER);
-        token.set_network(NETWORK_B, TOKEN_B);
+        token.set_network(NETWORK, TOKEN);
 
-        vm.prank(USER_A);
+        vm.prank(USER);
         vm.expectPartialRevert(IERC20Errors.ERC20InsufficientBalance.selector);
-        token.send(NETWORK_B, USER_B, 100500);
+        token.send(NETWORK, OWNER, AMOUNT);
 
         vm.prank(OWNER);
-        token.transfer(USER_A, 100500);
+        token.transfer(USER, AMOUNT);
 
-        vm.prank(USER_A);
-        token.send(NETWORK_B, USER_B, 100500);
-        assertEq(token.balanceOf(USER_A), 0);
-        assertEq(token.totalSupply(), CAP / 2 - 100500);
+        vm.prank(USER);
+        token.send(NETWORK, OWNER, AMOUNT);
+        assertEq(token.balanceOf(USER), 0);
+        assertEq(token.totalSupply(), CAP / 2 - AMOUNT);
     }
 }
